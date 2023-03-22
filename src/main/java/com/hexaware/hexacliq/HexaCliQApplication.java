9eddaf2cc1d34fd43e040f095f5d8088d1b92819
Kedar Kedar joshi <1000072115@hexaware.com>
2023-03-22 12:36:30 +0530

    Hexa-001 Intial checkin

---
+++ src/main/java/com/hexaware/hexacliq/HexaCliQApplication.java
@@ -0,0 +1,13 @@
+package com.hexaware.hexacliq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexaCliQApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexaCliQApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hexaware/hexacliq/HexaCliQApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexaware.hexacliq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexaCliQApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
