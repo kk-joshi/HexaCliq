295cc996f45ce784c79b29fd9b347c7764661e1c
Kedar Joshi <kedark@hexaware.com>
2023-04-12 23:02:48 +0530

    Report module changes

--- backend/src/main/java/com/hexaware/hexacliq/dto/CategoryEnum.java
+++ backend/src/main/java/com/hexaware/hexacliq/dto/CategoryEnum.java
@@ -1,13 +1,25 @@
 package com.hexaware.hexacliq.dto;
 
+import org.apache.poi.xssf.usermodel.XSSFColor;
+
 import java.util.Arrays;
 
-public enum CategoryEnum {
-    HALF_DAY("HD"), FULL_DAY("W"), OVERTIME("OT"), LEAVE("EL"), HOLIDAY("HOLIDAY"), OTHER("UNKNOWN");
-    private final String shortName;
+import static com.hexaware.hexacliq.utils.Constants.*;
 
-    CategoryEnum(String shortName) {
+public enum CategoryEnum {
+    HALF_DAY("1/2", COLOR_MAROON),
+    FULL_DAY("W", COLOR_NAVY),
+    OVERTIME("OT", COLOR_PURPLE),
+    LEAVE("EL", COLOR_MAROON),
+    HOLIDAY("HD", COLOR_MAROON),
+    FURLOUGH("FUR", COLOR_MAROON),
+    OTHER("--", COLOR_GRAY);
+    private final String shortName;
+    private final XSSFColor color;
+
+    CategoryEnum(String shortName, XSSFColor color) {
         this.shortName = shortName;
+        this.color = color;
     }
 
     public static CategoryEnum getCategoryBy(String category) {
@@ -22,5 +34,7 @@ public enum CategoryEnum {
         return shortName;
     }
 
-
+    public XSSFColor getColor() {
+        return color;
+    }
 }
