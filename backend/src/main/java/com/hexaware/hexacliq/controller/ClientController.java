9111ea55b54aebfe4175baba74134c51f5520f60
Kedar Joshi <kedark@hexaware.com>
2023-04-14 18:00:12 +0530

    Report module changes final changes

--- backend/src/main/java/com/hexaware/hexacliq/controller/ClientController.java
+++ backend/src/main/java/com/hexaware/hexacliq/controller/ClientController.java
@@ -1,12 +1,17 @@
 package com.hexaware.hexacliq.controller;
 
 import com.hexaware.hexacliq.dto.Client;
+import com.hexaware.hexacliq.dto.User;
 import com.hexaware.hexacliq.exception.DataNotFoundException;
 import com.hexaware.hexacliq.service.ClientService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 
 @RestController
 @RequestMapping("/clients")
@@ -17,13 +22,21 @@ public class ClientController {
     @Autowired
     ClientService clientService;
 
-    @PostMapping("/")
-    public ResponseEntity<Object> createClient(@RequestBody Client client) {
+    @PostMapping
+    public ResponseEntity<Object> createClient(@RequestBody Client client, Authentication authentication) {
+        User user = (User) authentication.getPrincipal();
+        client.setCreatedBy(user.getUsername());
+        client.setCreatedTime(LocalDate.now());
+        client.setModifiedBy(user.getUsername());
+        client.setModifiedTime(LocalDate.now());
         return ResponseEntity.ok(clientService.save(client));
     }
 
-    @PutMapping("/")
-    public ResponseEntity<Object> updateClient(@RequestBody Client client) {
+    @PutMapping
+    public ResponseEntity<Object> updateClient(@RequestBody Client client, Authentication authentication) {
+        User user = (User) authentication.getPrincipal();
+        client.setModifiedBy(user.getUsername());
+        client.setModifiedTime(LocalDate.now());
         return ResponseEntity.ok(clientService.save(client));
     }
 
@@ -31,7 +44,7 @@ public class ClientController {
     public ResponseEntity<Object> findClient(@PathVariable Integer clientId) {
         Client client = clientService.findById(clientId)
                 .orElseThrow(() -> new DataNotFoundException("Client not found"));
-        return ResponseEntity.ok(client);
+        return new ResponseEntity<>(client, HttpStatus.CREATED);
     }
 
 }
--- backend/src/main/java/com/hexaware/hexacliq/controller/ProjectController.java
+++ backend/src/main/java/com/hexaware/hexacliq/controller/ProjectController.java
@@ -1,12 +1,17 @@
 package com.hexaware.hexacliq.controller;
 
 import com.hexaware.hexacliq.dto.Project;
+import com.hexaware.hexacliq.dto.User;
 import com.hexaware.hexacliq.exception.DataNotFoundException;
 import com.hexaware.hexacliq.service.ProjectService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 
 @RestController
 @RequestMapping("/projects")
@@ -16,21 +21,29 @@ public class ProjectController {
     @Autowired
     ProjectService projectService;
 
-    @PostMapping("/")
-    public ResponseEntity<Object> createProject(@RequestBody Project Project) {
-        return ResponseEntity.ok(projectService.save(Project));
+    @PostMapping
+    public ResponseEntity<Object> createProject(@RequestBody Project project, Authentication authentication) {
+        User user = (User) authentication.getPrincipal();
+        project.setCreatedBy(user.getUsername());
+        project.setCreatedTime(LocalDate.now());
+        project.setModifiedBy(user.getUsername());
+        project.setModifiedTime(LocalDate.now());
+        return ResponseEntity.ok(projectService.save(project));
     }
 
-    @PutMapping("/")
-    public ResponseEntity<Object> updateProject(@RequestBody Project Project) {
-        return ResponseEntity.ok(projectService.save(Project));
+    @PutMapping
+    public ResponseEntity<Object> updateProject(@RequestBody Project project, Authentication authentication) {
+        User user = (User) authentication.getPrincipal();
+        project.setModifiedBy(user.getUsername());
+        project.setModifiedTime(LocalDate.now());
+        return ResponseEntity.ok(projectService.save(project));
     }
 
     @GetMapping("/{projectId}")
     public ResponseEntity<Object> findProject(@PathVariable Integer projectId) {
-        Project Project = projectService.findById(projectId)
+        Project project = projectService.findById(projectId)
                 .orElseThrow(() -> new DataNotFoundException("Project not found"));
-        return ResponseEntity.ok(Project);
+        return new ResponseEntity<>(project, HttpStatus.CREATED);
     }
 
 }
